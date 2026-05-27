b2857f2a7bfc5041b575853f22a2776e172f625d
iwnelion <anname17@hotmail.com>
2026-05-27 18:51:21 +0300

    added polis events to the projects array

---
+++ projects.js
@@ -0,0 +1,16 @@
+// projects array with objects as individual projects
+let projects = [
+    // Polis Events
+    {
+        id: 1,
+        title: "Polis Events",
+        category: "web-frontend",
+        type: "[In Progress]",
+        featured: true,
+        shortDescription: "Custom WordPress events platform that replaces standard blog posts with a dynamic event management and filtering system, allowing users to search events by category, location, date, age limits, and tags.",
+        technologies: ["WordPress", "ACF", "The Events Calendar", "Elementor", "PHP", "CSS"],
+        // image: 
+        // liveUrl: 
+        caseStudy: true
+    }
+]
