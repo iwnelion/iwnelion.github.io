3390399412e1dbe8d10e020688543922f58e4a82
iwnelion <anname17@hotmail.com>
2026-05-29 17:59:59 +0300

    made a forEach loop to render the wp projects

--- render.js
+++ render.js
@@ -1,13 +1,14 @@
 // renders the content from projects.js to the project pages
-let projectsContainer = document.querySelector(".projects-container")
+let wordpressContainer = document.querySelector("#wordpress-projects");
+let learningContainer = document.querySelector("#learning-projects");
 
-let webProjects = projects.filter(p => p.category === "web-frontend")
-let project = webProjects[1]
-let projectHTML = `
-  <div class="project-card">
-    <h3>${project.title}</h3>
-    <p>${project.shortDescription}</p>
-  </div>
-`;
-
-projectsContainer.innerHTML += projectHTML
+let wordpressProjects = projects.filter(p => p.category === "web-frontend" && p.subcategory === "wordpress")
+wordpressProjects.forEach(project => {
+    let projectHTML = `
+        <div class="project-card">
+            <h3>${project.title}</h3>
+            <p>${project.shortDescription}</p>
+        </div>
+    `
+    projectsContainer.innerHTML += projectHTML;
+})
