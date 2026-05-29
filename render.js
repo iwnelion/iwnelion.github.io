7e7a0b7550e8f87dbe0e31f9d3685596ffc80f0d
iwnelion <anname17@hotmail.com>
2026-05-29 18:17:44 +0300

    made a forEach for all project types

--- render.js
+++ render.js
@@ -1,14 +1,30 @@
 // renders the content from projects.js to the project pages
+function renderProjects(projectsArray, container){
+    container.innerHTML = "";
+    let output = "";
+
+    projectsArray.forEach(project => {
+        output += `
+            <div class="project-card">
+                <h3>${project.title}</h3>
+                <p>${project.shortDescription}</p>
+            </div>
+        `;
+    });
+
+    container.innerHTML = output;
+}
+
 let wordpressContainer = document.querySelector("#wordpress-projects");
 let learningContainer = document.querySelector("#learning-projects");
 
-let wordpressProjects = projects.filter(p => p.category === "web-frontend" && p.subcategory === "wordpress")
-wordpressProjects.forEach(project => {
-    let projectHTML = `
-        <div class="project-card">
-            <h3>${project.title}</h3>
-            <p>${project.shortDescription}</p>
-        </div>
-    `
-    projectsContainer.innerHTML += projectHTML;
-})
+let wordpressProjects = projects.filter(
+  p => p.category === "web-frontend" && p.subcategory === "wordpress"
+);
+
+let learningProjects = projects.filter(
+  p => p.category === "web-frontend" && p.subcategory === "learning"
+);
+
+renderProjects(wordpressProjects, wordpressContainer);
+renderProjects(learningProjects, learningContainer);
